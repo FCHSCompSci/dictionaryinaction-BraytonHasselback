a8b36208d8df7fed8888eeaed45c15f00ac67bda
Brayton Thomas Hasselback <brayton.hasselback@stu.jefferson.kyschools.us>
2018-09-18 07:53:40 -0400

    first attempt at book catalog

---
+++ book catalog.py
@@ -0,0 +1,12 @@
+catalog = [""]
+
+
+def book(title, author, genre):
+    bookadd = input("Would you like to put your book into the catalog? (Yes / No)")
+    if bookadd == ("Yes"):
+           catalog.append(book)
+    else:
+        return;
+        
+    
+        
